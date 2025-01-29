36fa00c8f35c040c97ddeef231bc11b6a4b1f085
jsoulier <jaansoulier@gmail.com>
2025-01-29 16:32:20 -0500

    camera positioning

--- src/config.h
+++ src/config.h
@@ -6,15 +6,14 @@
 #define HEIGHT 225.0f
 #define SAMPLES 100
 #define DEPTH 50
-#define VIEWPORT_SIZE 0.01f
-#define FOCAL_LENGTH 1.0f
+#define SOURCE vec3(-2.0f, 2.0f, 1.0f)
+#define TARGET vec3(0.0f, 0.0f, -1.0f)
+#define UP vec3(0.0f, 1.0f, 0.0f)
+#define FOV 90
 #define IMAGE "image.bmp"
 
 #define LAMBERTIAN 0
 #define METAL 1
 #define DIAELECTRIC 2
-
-#define IOR_AIR 1.0f
-#define IOR_WATER 1.33f
 
 #endif
