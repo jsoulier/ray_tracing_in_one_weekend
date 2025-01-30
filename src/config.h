91956bfaf4f5053d36d7a6d9b26ce07e9e198e6f
jsoulier <jaansoulier@gmail.com>
2025-01-29 22:23:43 -0500

    refactor

--- src/config.h
+++ src/config.h
@@ -6,13 +6,15 @@
 #define HEIGHT 450.0f
 #define SAMPLES 200
 #define DEPTH 50
+#define FOV 20
 #define SOURCE vec3(13.0f, 2.0f, 3.0f)
 #define TARGET vec3(0.0f, 0.0f, -1.0f)
 #define UP vec3(0.0f, 1.0f, 0.0f)
-#define FOV 20
-#define DEFOCUS_ANGLE 0.6f
-#define FOCUS_LENGTH 10.0f
+#define FOCUS 10.0f
+#define OOF 0.6f
 #define IMAGE "image.bmp"
+#define SKY vec3(0.5f, 0.7f, 1.0f)
+#define HORIZON vec3(1.0f, 1.0f, 1.0f)
 
 #define LAMBERTIAN 0
 #define METAL 1
